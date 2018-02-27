eda059a4bb72f6c98d4b7c428e018c7028b50aff
Sudheer Kumar Anyam <sudheer.anyam@arthdesignbuild.com>
2018-02-27 17:33:24 +0530

    Implemented Drawer Layout  by using react-navigation module

--- App.js
+++ App.js
@@ -1,59 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from "react";
+import {View, StyleSheet, Text, TextInput, Button, Platform, ScrollView } from "react-native";
+import {DrawerNavigator} from 'react-navigation';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import firstScreen from './screens/firstScreen';
+import secondScreen from './screens/secondScreen';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native Sudheer Kumar!npm install -g eslint
-          
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+const drawerExample = DrawerNavigator(
+  {
+    First: {
+      path: '/',
+      screen: firstScreen,
+    },
+    Second: {
+      path: '/sent',
+      screen: secondScreen,
+    }
+  },
+  {
+    initialRoutName: "First",
+    drawerPosition:'left'
   }
-}
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default drawerExample;
+
